427cda463fd48ae8a3a22ed0c518311b3190c343
Marcos Parreiras <marcosalp1998@gmail.com>
2023-07-20 17:00:58 -0300

    Adjusted styles and zoom

--- pages/index.js
+++ pages/index.js
@@ -66,8 +66,9 @@ export default function Home() {
                 />
                 <meta
                     name='viewport'
-                    content='width=device-width, initial-scale=1'
+                    content='width=device-width, initial-scale=1, maximum-scale=1'
                 />
+
                 <link rel='icon' href='/favicon.ico' />
             </Head>
             <div className='App'>
